26e22fafb4ca9555383bcc2f5b74cc10f29c7bb7
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-04-25 16:05:52 -0300

    Fix date utc

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -40,14 +40,18 @@ class AvailableController {
             '19:00',
         ];
 
+        const requestDate = parseISO(date);
+
         const available = schedule.map((time) => {
             const [hour, minute] = time.split(':');
-            const value = setSeconds(setMinutes(setHours(searchDate, hour), minute), 0);
+            const value = setSeconds(setMinutes(setHours(requestDate, hour), minute), 0);
+
+            const compareDate = zonedTimeToUtc(value, 'America/Sao_Paulo');
 
             return {
                 time,
                 value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-                available: isAfter(value, new Date())
+                available: isAfter(compareDate, new Date())
                 && !appointments.find((a) => format(a.date, 'HH:mm') === time),
             };
         });
